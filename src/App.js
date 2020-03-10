dea6c2a7d8ef083adc121d39791a9b6819b27d16
Teresita2201 <60665548+Teresita2201@users.noreply.github.com>
2020-03-10 10:40:18 -0600

    agregar archivos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Head from './componentes/head'
+import Cuerpo from './componentes/dearrollo'
+
+class App extends React.Component {
+  render(){
+     return (
+       <div className="App container" >
+         <Head/>
+         <Cuerpo/>
+
+      
+       </div>
+      );
+  }
+}
+
+export default App;
---
+++ src/componentes/dearrollo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './comp.css';
+class Cuerpo extends Component {
+    render() {
+        return (
+            
+           <div className="Formulario">
+                <form method="post" action="https://es.reactjs.org/docs/getting-started.html">
+                <fieldset>
+                    <legend><h1>TaxiFAst</h1></legend>
+
+                    <label for="username">Correo Electronico</label><br/>
+                    <input type="text" name="username" id ="username"
+                        placeholder="example@gmail.com" size="30" maxlenght="15" required/>
+                        <br />
+                        <label for="pass"> contraseña</label><br/>
+                        <input type="password" name="pass" id="pass" placeholder="**************" size="2" maxlenght="15"required/>
+                        <br/><button type="button" class="btn btn-link">¿Olvidaste tu contraseña?</button>
+                </fieldset>
+                    
+                    <p>
+                        <input name="recordar" type="checkbox" />Recordar contraseña
+                    </p>
+                
+                <input type="submit" value="Enviar"></input>  
+                <button type="button" class="btn btn-link">Registrarse</button>
+
+            </form>
+            </div>
+        );
+    }
+}
+
+export default Cuerpo;
